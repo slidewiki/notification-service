b7c10f5e2123a0c46d857c4df2719d90993b930f
Kurt Junghanns <tboonx@googlemail.com>
2016-02-18 13:05:32 +0100

    Fixed configuration.js.

--- application/configuration.js
+++ application/configuration.js
@@ -4,18 +4,18 @@
 let host = 'localhost';
 var fs = require('fs');
 var lines = fs.readFileSync('/etc/hosts').toString().split("\n");
-for(var i in lines) {
-    if (lines[i].startsWith('::1')) {
-      const entrys = lines[i].split(' ');
-      host = entrys[entrys.length - 1];
-      console.log('found mongodb host', host);
-    }
+for (var i in lines) {
+  if (lines[i].startsWith('mongodb')) {
+    const entrys = lines[i].split(' ');
+    host = entrys[entrys.length - 1];
+    console.log('found mongodb host', host);
+  }
 }
 
-module.exports =  {
+module.exports = {
   MongoDB: {
-    URL: 'mongodb://localhost:27017/local',
-    PORT: 27017,
+    URL: 'mongodb://localhost:27018/local',
+    PORT: 27018,
     HOST: host,
     NS: 'local',
     SLIDEWIKIDATABASE: 'slidewiki'

352e9f6cf915f326e7aaec076869070d83922e40
Kurt Junghanns <tboonx@googlemail.com>
2016-02-18 13:02:26 +0100

    Changed configuration.js to read mongodb host/ip from hostsfile.

--- application/configuration.js
+++ application/configuration.js
@@ -1,10 +1,22 @@
 'use strict';
 
+//read mongodb URL from /etc/hosts
+let host = 'localhost';
+var fs = require('fs');
+var lines = fs.readFileSync('/etc/hosts').toString().split("\n");
+for(var i in lines) {
+    if (lines[i].startsWith('::1')) {
+      const entrys = lines[i].split(' ');
+      host = entrys[entrys.length - 1];
+      console.log('found mongodb host', host);
+    }
+}
+
 module.exports =  {
   MongoDB: {
-    URL: 'mongodb://localhost:27018/local',
-    PORT: 27018,
-    HOST: 'localhost',
+    URL: 'mongodb://localhost:27017/local',
+    PORT: 27017,
+    HOST: host,
     NS: 'local',
     SLIDEWIKIDATABASE: 'slidewiki'
   }

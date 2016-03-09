ad0b3691073ed9ccc9c166a59977ad91bc533e4c
Kurt Junghanns <tboonx@googlemail.com>
2016-03-09 11:21:52 +0100

    Fixed unit tests

--- application/tests/unit_dbimitation.js
+++ application/tests/unit_dbimitation.js
@@ -29,43 +29,51 @@ describe('Database', () => {
 
     it('should return the slide when inserting one', () => {
       let slide = {
-        name: 'Foo'
+        title: 'Dummy',
+        body: 'dummy',
+        language: 'en'
       };
       let res = db.insert(slide);
       return Promise.all([
         res.should.be.fulfilled.and.eventually.not.be.empty,
         res.should.eventually.have.property('ops').that.is.not.empty,
-        res.should.eventually.have.deep.property('ops[0]').that.has.all.keys('_id', 'name'),
-        res.should.eventually.have.deep.property('ops[0].name', slide.name)
+        res.should.eventually.have.deep.property('ops[0]').that.has.all.keys('_id', 'title', 'body', 'language'),
+        res.should.eventually.have.deep.property('ops[0].title', slide.title)
       ]);
     });
 
     it('should get an previously inserted slide', () => {
       let slide = {
-        name: 'Foo'
+        title: 'Dummy',
+        body: 'dummy',
+        language: 'en'
       };
       let ins = db.insert(slide);
       let res = ins.then((ins) => db.get(ins.ops[0]._id));
       return Promise.all([
         res.should.be.fulfilled.and.eventually.not.be.empty,
-        res.should.eventually.have.all.keys('_id', 'name'),
+        res.should.eventually.have.all.keys('_id', 'title', 'body', 'language'),
         ins.then((slide) => res.should.eventually.deep.equal(slide.ops[0]))
       ]);
     });
 
     it('should be able to replace an previously inserted slide', () => {
       let slide = {
-        name: 'Foo'
+        title: 'Dummy',
+        body: 'dummy',
+        language: 'en'
       };
       let slide2 = {
-        name: 'Bar'
+        title: 'Dummy2',
+        body: 'dummyish',
+        language: 'en?'
       };
       let ins = db.insert(slide);
       let res = ins.then((ins) => db.replace(ins.ops[0]._id, slide2));
       return Promise.all([
         res.should.be.fulfilled.and.eventually.not.be.empty,
-        res.should.eventually.have.property('value').that.contains.all.keys('_id', 'name'),
-        res.should.eventually.have.property('value').that.has.property('name', slide.name) // FIXME returns old object
+        res.should.eventually.have.property('value').that.contains.all.keys('_id', 'title', 'body', 'language'),
+        res.should.eventually.have.property('value').that.has.property('title', slide.title) // FIXME returns old object
         //ins.then((slide) => res.should.eventually.have.deep.property('value._id', slide.ops[0]._id))//FIXME works, but fails because of .... santa
       ]);
     });

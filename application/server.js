789e3967d906a13ae5bafc876fc60abea2391e71
dpaun <dpaunovic@gmail.com>
2016-08-17 16:01:40 +0200

    Changed info title

--- application/server.js
+++ application/server.js
@@ -49,7 +49,7 @@ let plugins = [
     options: {
       host: host,
       info: {
-        title: 'Example API',
+        title: 'Notification Management API',
         description: 'Powered by node, hapi, joi, hapi-swaggered, hapi-swaggered-ui and swagger-ui',
         version: '0.1.0'
       }

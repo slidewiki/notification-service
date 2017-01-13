67432bb4924af8a50db3ac5dc4bcadd569c4dd94
dpaun <dpaunovic@gmail.com>
2017-01-13 12:21:59 +0100

    Removed pieces of code which worked with mockup data

--- application/controllers/handler.js
+++ application/controllers/handler.js
@@ -18,10 +18,6 @@ module.exports = {
         reply(boom.notFound());
       else {
         return insertAuthor(notification).then((notification) => {
-
-          if (notification.user_id.length === 24) {//Mockup - old kind of ids
-            notification.author = getMockupAuthor(notification.user_id);
-          }
           reply(co.rewriteID(notification));
         }).catch((error) => {
           request.log('error', error);
@@ -91,23 +87,13 @@ module.exports = {
 
   //Get All notifications from database for the id in the request
   getNotifications: function(request, reply) {
-    //Clean collection and insert mockup notifications - only if request.params.id === 0
-    return initMockupData(request.params.id)
-      .then(() => notificationsDB.getAllFromCollection()//TODO call getAllWithSubscribedUserID(identifier)
-      // .then(() => notificationsDB.getAllWithSubscribedUserID(encodeURIComponent(request.params.id))
+    return notificationsDB.getAllFromCollection()//TODO call getAllWithSubscribedUserID(identifier)
+    //return notificationsDB.getAllWithSubscribedUserID(encodeURIComponent(request.params.id))
       .then((notifications) => {
         let arrayOfAuthorPromisses = [];
         notifications.forEach((notification) => {
           co.rewriteID(notification);
-          let promise = insertAuthor(notification).then((notification) => {
-
-            if (notification.user_id.length === 24) {//Mockup - old kind of ids
-              notification.author = getMockupAuthor(notification.user_id);//insert author data
-            }
-          }).catch((error) => {
-            request.log('error', error);
-            reply(boom.badImplementation());
-          });
+          let promise = insertAuthor(notification);
           arrayOfAuthorPromisses.push(promise);
         });
         Promise.all(arrayOfAuthorPromisses).then(() => {
@@ -118,11 +104,6 @@ module.exports = {
           request.log('error', error);
           reply(boom.badImplementation());
         });
-
-
-      })).catch((error) => {
-        request.log('error', error);
-        reply(boom.badImplementation());
       });
   },
 
@@ -133,15 +114,7 @@ module.exports = {
         let arrayOfAuthorPromisses = [];
         notifications.forEach((notification) => {
           co.rewriteID(notification);
-          let promise = insertAuthor(notification).then((notification) => {
-
-            if (notification.user_id.length === 24) {//Mockup - old kind of ids
-              notification.author = getMockupAuthor(notification.user_id);//insert author data
-            }
-          }).catch((error) => {
-            request.log('error', error);
-            reply(boom.badImplementation());
-          });
+          let promise = insertAuthor(notification);
           arrayOfAuthorPromisses.push(promise);
         });
 
@@ -205,128 +178,3 @@ function insertAuthor(notification) {
 
   return myPromise;
 }
-
-function getMockupAuthor(userId) {
-  let author = authorsMap.get(userId);//insert author data
-  if (author === undefined) {
-    author = authorsMap.get('112233445566778899000000');
-  }
-  return author;
-}
-
-//Delete all and insert mockup data
-function initMockupData(identifier) {
-  if (identifier === '000000000000000000000000') {//create collection, delete all and insert mockup data only if the user has explicitly sent 000000000000000000000000
-    return notificationsDB.createCollection()
-      .then(() => notificationsDB.deleteAll())
-      .then(() => insertMockupData());
-  }
-  return new Promise((resolve) => {resolve (1);});
-}
-
-
-
-// function getFirstTwoActivities() {
-//   let http = require('http');
-//   const Microservices = require('../configs/microservices');
-//   let options = {
-//     //CHANGES FOR LOCALHOST IN PUPIN (PROXY)
-//     // host: 'proxy.rcub.bg.ac.rs',
-//     // port: 8080,
-//     // path: 'http://activitiesservice.manfredfris.ch/activity/new',
-//     // path: 'http://' + Microservices.activities.uri + '/activity/new',
-//
-//     // host: 'activitiesservice.manfredfris.ch',
-//     host: Microservices.activities.uri,
-//     port: 80,
-//     path: '/activities/8',
-//   };
-//
-//   let req = http.get(options, (res) => {
-//     // console.log('STATUS: ' + res.statusCode);
-//     // console.log('HEADERS: ' + JSON.stringify(res.headers));
-//     let body = '';
-//     res.on('data', (chunk) => {
-//       body += chunk;
-//     });
-//     res.on('end', () => {
-//       let data = JSON.parse(body);
-//       console.log(data);
-//       return data;
-//     });
-//   });
-//   req.on('error', (e) => {
-//     console.log('problem with request: ' + e.message);
-//   });
-//
-// }
-//Insert mockup data to the collection
-function insertMockupData() {
-  //get real ids for the first 2 activities
-  // const firstTwoActivities = getFirstTwoActivities();
-  const firstTwoActivities = [
-    {activity_id: '57b6c4eb000eee001048bd26'},
-    {activity_id: '57b6c4eb000eee001048bd25'}
-  ];
-  let notification1 = {
-    activity_id: firstTwoActivities[0].activity_id,
-    activity_type: 'react',
-    content_id: '8',
-    content_kind: 'slide',
-    content_name: 'Introduction',
-    user_id: '112233445566778899000002',
-    react_type: 'like',
-    subscribed_user_id: '112233445566778899000001'
-  };
-  let ins1 = notificationsDB.insert(notification1);
-  let notification2 = {
-    activity_id: firstTwoActivities[1].activity_id,
-    activity_type: 'download',
-    content_id: '8',
-    content_kind: 'slide',
-    content_name: 'Introduction',
-    user_id: '112233445566778899000001',
-    subscribed_user_id: '112233445566778899000001'
-  };
-  let ins2 = ins1.then(() => notificationsDB.insert(notification2));
-
-  return ins2;
-}
-
-let authorsMap = new Map([
-  ['112233445566778899000001', {
-    id: 7,
-    username: 'Dejan P.',
-    avatar: '/assets/images/mock-avatars/deadpool_256.png'
-  }],
-  ['112233445566778899000002', {
-    id: 8,
-    username: 'Nikola T.',
-    avatar: '/assets/images/mock-avatars/man_512.png'
-  }],
-  ['112233445566778899000003', {
-    id: 9,
-    username: 'Marko B.',
-    avatar: '/assets/images/mock-avatars/batman_512.jpg'
-  }],
-  ['112233445566778899000004', {
-    id: 10,
-    username: 'Valentina J.',
-    avatar: '/assets/images/mock-avatars/ninja-simple_512.png'
-  }],
-  ['112233445566778899000005', {
-    id: 11,
-    username: 'Voice in the crowd',
-    avatar: '/assets/images/mock-avatars/anon_256.jpg'
-  }],
-  ['112233445566778899000006', {
-    id: 12,
-    username: 'SlideWiki FTW',
-    avatar: '/assets/images/mock-avatars/spooky_256.png'
-  }],
-  ['112233445566778899000000', {
-    id: 13,
-    username: 'Dutch',
-    avatar: '/assets/images/mock-avatars/dgirl.jpeg'
-  }]
-]);

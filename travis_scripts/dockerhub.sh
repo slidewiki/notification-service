54fa6926c0587568f6ce1a3a92c0c5634c449b46
Ben Wulff <benjamin.wulff@iais.fraunhofer.de>
2017-06-13 04:09:50 +0200

    Using the latest-dev tag for development  (experimental)

--- travis_scripts/dockerhub.sh
+++ travis_scripts/dockerhub.sh
@@ -2,4 +2,4 @@
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build -t slidewiki/notificationservice ./
-docker push slidewiki/notificationservice
+docker push slidewiki/notificationservice:latest-dev

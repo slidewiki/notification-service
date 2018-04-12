cdfb2b43f5c41396c413e688859a9b6f73e29546
Roy Meissner <meissner@informatik.uni-leipzig.de>
2018-04-12 12:25:41 +0200

    Changed the way Travis logs in into docker

--- travis_scripts/dockerhub.sh
+++ travis_scripts/dockerhub.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+echo $DOCKER_PASSWORD | docker login -u="$DOCKER_USERNAME" --password-stdin
 docker build --build-arg BUILD_ENV=travis -t slidewiki/microservicetemplate:latest-dev ./
 docker push slidewiki/microservicetemplate:latest-dev
